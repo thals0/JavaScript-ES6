a2d3f7ddd8d2695ce3bb7835931bb456a40e24c2
SoMin <thals0107@naver.com>
2022-09-18 17:09:09 +0900

    Study this and arrow function

---
+++ index.js
@@ -0,0 +1,33 @@
+// this & arrow function 연습문제 1
+// const person = {
+//   name: "손흥민",
+//   sayHi: function (name) {
+//     console.log("안녕 나는" + this.name);
+//   },
+// };
+
+// person.sayHi();
+
+// this & arrow function 연습문제 2
+let dataSet = {
+  data: [1, 2, 3, 4, 5],
+};
+
+dataSet.plus = function () {
+  let sum = 0;
+  this.data.forEach(function (a) {
+    sum += a;
+  });
+  console.log(sum);
+};
+dataSet.plus();
+
+// this & arrow function 연습문제 3
+// setTimeout(function () {
+//   console.log("hi");
+// }, 1000);
+
+// function 함수() {
+//   console.log("hi");
+// }
+// setTimeout(함수, 1000);
